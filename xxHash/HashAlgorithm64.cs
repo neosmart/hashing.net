cc15bbe73c752dc7d8d782a83c7835288ff925c3
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-07-18 13:54:54 -0500

    Initial port of Wilhelm Liao's xxHash for C# to .NET Core
    Based off of https://github.com/wilhelmliao/xxHash.NET Originally developed by
    Yann Collet: https://github.com/Cyan4973/xxHash

---
+++ xxHash/HashAlgorithm64.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+
+namespace NeoSmart.Hashing.XXHash
+{
+    public class HashAlgorithm64 : HashAlgorithm
+    {
+        private UInt64 hash;
+
+        public override int HashSize => 64; //in bits
+
+        public override void Initialize()
+        {
+        }
+
+        protected override void HashCore(byte[] array, int ibStart, int cbSize)
+        {
+            var state = XXHash.CreateState64();
+            bool result = XXHash.UpdateState64(state, array, ibStart, cbSize);
+            Debug.Assert(result, "Internal xxhash failure!");
+            hash = XXHash.DigestState64(state);
+        }
+
+        protected override byte[] HashFinal()
+        {
+            return BitConverter.GetBytes(hash);
+        }
+    }
+}
